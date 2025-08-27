27e33bb393c65b5f6e70dc2f17bd6dfc79b56119
Mike <michaelnabebe@gmail.com>
2025-08-27 14:09:34 -0400

    wsl updates w/ Makefile

--- Makefile
+++ Makefile
@@ -1,15 +1,15 @@
 CC = gcc
 CFLAGS = -Wall -Wextra -Iinclude -g
 
-SRC_DIR = src
-OBJ_DIR = obj
-INCLUDE_DIR = include
-BIN_DIR = bin
+SRC_DIR = ./src
+OBJ_DIR = ./obj
+INCLUDE_DIR = ./include
+BIN_DIR = ./bin
 
 TARGET = $(BIN_DIR)/myhttpd
 
-SRC_FILES = $(wildcard $(SRC_DIR)/*c)
-OBJ_FILES = $(patsubst $(SRC_DIR)/%.c, $(OBJ_DIR)/%.o, $(SRC_FILES)/%.c)
+SRC_FILES = $(wildcard $(SRC_DIR)/*.c)
+OBJ_FILES = $(patsubst $(SRC_DIR)/%.c, $(OBJ_DIR)/%.o, $(SRC_FILES))
 
 all: $(TARGET)
 
@@ -17,7 +17,7 @@ $(TARGET): $(OBJ_FILES) | $(BIN_DIR)
 	$(CC) $(CFLAGS) -o $@ $^
 
 $(OBJ_DIR)/%.o: $(SRC_DIR)/%.c | $(OBJ_DIR)
-	$(CC) $(CFLAGS) -o $@ $^
+	$(CC) $(CFLAGS) -c $< -o $@
 
 $(OBJ_DIR):
 	mkdir -p $(OBJ_DIR)
--- src/main.c
+++ src/main.c
@@ -1,24 +1,24 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
 #include "tcp.h"
 
+
 int main() {
+    printf("START\n");
     tcp_server server = {0};
     server_status_e status = bind_tcp_port(&server, 8080);
     if (status != SERVER_OK) {
-        printf("Server initialization failed");
+        printf("Server initialization failed\n");
         exit(EXIT_FAILURE);
     }
 
     int client_fd = accept_client(server.socket_fd);
     if (client_fd == -1) {
-        printf("Failed to accept client connection");
+        printf("Failed to accept client connection\n");
         close(server.socket_fd);
         exit(EXIT_FAILURE);
     }
 
-    printf("Client connected");
+    printf("Client connected\n");
 
     close(client_fd);
     close(server.socket_fd);
